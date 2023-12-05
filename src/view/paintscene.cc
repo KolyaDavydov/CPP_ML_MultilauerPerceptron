889f2eb3fc5e55dea4944b820f1d75593e07b93f
nohoteth <nohoteth@student.21-school.ru>
2023-12-05 21:10:36 +0300

    brush width for Mac scale

--- src/view/paintscene.cc
+++ src/view/paintscene.cc
@@ -13,12 +13,8 @@ paintScene::~paintScene()
 void paintScene::mousePressEvent(QGraphicsSceneMouseEvent *event)
 {
     // При нажатии кнопки мыши отрисовываем эллипс
-    addEllipse(event->scenePos().x() - 5,
-               event->scenePos().y() - 5,
-               10,
-               10,
-               QPen(Qt::NoPen),
-               QBrush(Qt::white));
+    addEllipse(event->scenePos().x() - 20, event->scenePos().y() - 20, 40, 40,
+               QPen(Qt::NoPen), QBrush(Qt::white));
     // Сохраняем координаты точки нажатия
     previousPoint = event->scenePos();
 }
@@ -26,11 +22,9 @@ void paintScene::mousePressEvent(QGraphicsSceneMouseEvent *event)
 void paintScene::mouseMoveEvent(QGraphicsSceneMouseEvent *event)
 {
     // Отрисовываем линии с использованием предыдущей координаты
-    addLine(previousPoint.x(),
-            previousPoint.y(),
-            event->scenePos().x(),
+    addLine(previousPoint.x(), previousPoint.y(), event->scenePos().x(),
             event->scenePos().y(),
-            QPen(Qt::white,10,Qt::SolidLine,Qt::RoundCap));
+            QPen(Qt::white, 40, Qt::SolidLine, Qt::RoundCap));
     // Обновляем данные о предыдущей координате
     previousPoint = event->scenePos();
 }
